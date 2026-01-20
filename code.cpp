852e9804ebe15e0c175298b970457bb0298a8bfd
李泉 <119789352+liquan2003@users.noreply.github.com>
2026-01-20 10:08:52 +0800

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,21 @@
+﻿/**
+ * code.cpp: 定义应用程序的入口点
+ * 流体仿真系统的主程序
+ */
+
+#include "code.h"
+#include "UI.h"
+
+using namespace std;
+
+/**
+ * 主函数
+ * 创建UI对象并启动仿真系统
+ * @return 程序退出码
+ */
+int main()
+{
+	FluidSimulation::UI ui;
+	ui.run();
+	return 0;
+}
